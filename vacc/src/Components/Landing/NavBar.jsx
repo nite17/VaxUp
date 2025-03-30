9a96d83be34fc12b8bf40191ac6c6b48c019638a
Vansh <vansh.chani.code@gmail.com>
2025-03-30 20:18:46 +0530

    minor style fix

--- vacc/src/Components/Landing/NavBar.jsx
+++ vacc/src/Components/Landing/NavBar.jsx
@@ -1,6 +1,6 @@
 export default function NavBar(){
     return(
-        <div class="flex h-20 justify-between w-full bg-white items-center">
+        <div class="flex h-25 justify-between w-full bg-white items-center">
         <p class="text-transparent bg-clip-text bg-gradient-to-r from-logofro to-logoto text-6xl ml-10 font-black">VaxUp</p>
         <div class="flex space-x-14 mr-16  ">
           <a href="#" class=" text-xl font-semibold text-navb mt-2 transition-all duration-200 hover:scale-110 no-underline">FAQ</a>
